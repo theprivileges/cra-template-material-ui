f5c91115e130b090e3e6ae23f9a56b53fac930b4
Luiz Lopes <luiz@wickeddeveloper.com>
2020-08-17 08:51:19 -0600

    feat(template): create material-ui template
    Adds functionality to generate a cra with material-ui as the main ui for the app.
    
    Template will include:
      - Standard JavaScript Style
      - husky
      - lint-staged

--- template/src/Copyright.js
+++ template/src/Copyright.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+import Link from '@material-ui/core/Link'
 
-export default function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright &copy; '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+export default function Copyright () {
+    return (
+        <Typography variant='body2' color='textSecondary' align='center'>
+            {'Copyright ©️ '}
+            <Link color='inherit' href='https://material-ui.com/'>
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    )
 }
--- template/src/theme.js
+++ template/src/theme.js
@@ -1,36 +1,36 @@
-import { red } from '@material-ui/core/colors';
-import { createMuiTheme } from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors'
+import { createMuiTheme } from '@material-ui/core/styles'
 
 // A custom theme for this app
 const theme = createMuiTheme({
-  typography: {
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
-  },
-  palette: {
-    primary: {
-      main: '#556cd6',
+    typography: {
+        fontFamily: [
+            '-apple-system',
+            'BlinkMacSystemFont',
+            '"Segoe UI"',
+            'Roboto',
+            '"Helvetica Neue"',
+            'Arial',
+            'sans-serif',
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"'
+        ].join(',')
     },
-    secondary: {
-      main: '#19857b',
-    },
-    error: {
-      main: red.A400,
-    },
-    background: {
-      default: '#fff',
-    },
-  },
-});
+    palette: {
+        primary: {
+            main: '#556cd6'
+        },
+        secondary: {
+            main: '#19857b'
+        },
+        error: {
+            main: red.A400
+        },
+        background: {
+            default: '#fff'
+        }
+    }
+})
 
-export default theme;
+export default theme
