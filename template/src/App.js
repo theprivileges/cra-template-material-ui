d9ea69a9939d05b6820aae097bd8e4021438060d
Luiz Lopes <luiz@wickeddeveloper.com>
2020-08-16 20:12:54 -0600

    feat: add material-ui shell template
    Adds simple matarial-ui shell template to be used by apps that use this cra-template.

---
+++ template/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Copyright from './Copyright';
+
+export default function App() {
+  return (
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+            Create React App using Material-UI
+        </Typography>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
